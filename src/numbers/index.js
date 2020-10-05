f4d81c9c6e6310779bbea0ee5dab0e95412bcb3e
Matt Coston <mattcoston7@gmail.com>
2020-10-05 14:27:58 -0500

    All tests Pass

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -19,7 +19,15 @@ const sum = (arr) => {
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  let arraySum = arr.reduce((accumulator, currentValue)=> {
+    return accumulator + currentValue
+  }, 0)
+  if (arraySum == sum){
+    return true
+    } else {
+      return false
+    }
+  
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -6,12 +6,14 @@ const split = (str, delim) => {
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+//split string into pairs of letters
+return str.match(/.{1,2}/g)
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  // returns str after it is split reversed into an array and joined back together
+  return str.split("").reverse().join("");
 }
 
 module.exports = {
